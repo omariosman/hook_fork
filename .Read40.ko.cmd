76fe04a8ed5f525d4466af93c5a6fd8510365f59
root <root@csce-3402.aucegypt.edu>
2021-03-23 08:41:35 +0200

    ex6

---
+++ .Read40.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/Exercise_6/Read40.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T /usr/src/linux-headers-4.19.0-13-common/scripts/module-common.lds  --build-id  -o /root/Exercise_6/Read40.ko /root/Exercise_6/Read40.o /root/Exercise_6/Read40.mod.o ;  true
---
+++ .tmp_versions/Read40.mod
@@ -0,0 +1,3 @@
+/root/Exercise_6/Read40.ko
+/root/Exercise_6/Read40.o
+
